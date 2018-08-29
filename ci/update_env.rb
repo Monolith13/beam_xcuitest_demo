1d7e3c18a9ba3b05ad3dc4b9cce3f8f05f20c700
Vladislav Zinger <vladislavzinger@gmail.com>
2018-08-29 18:07:36 +0400

    Update update_env.rb
    Fix getting version of Java for old JDK ≤8

--- ci/update_env.rb
+++ ci/update_env.rb
@@ -6,7 +6,7 @@ File.open('./environment.xml', 'w') do |f|
   doc.at_xpath("//parameter[name=\'OS\']/value").content = %x$sw_vers -productVersion$.match(/[\d]*\.[\d]*\.[\d]*/)
   doc.at_xpath("//parameter[name=\'XCode\']/value").content = %x$xcodebuild -version$.match(/[\d]*\.[\d]*\.[\d]*/)
   doc.at_xpath("//parameter[name=\'Ruby\']/value").content = %x$allure --version$.match(/[\d]*\.[\d]*\.[\d]*/)
-  doc.at_xpath("//parameter[name=\'Java\']/value").content = %x$java --version$.match(/[\d]*\.[\d]*\.[\d]*/)
+  doc.at_xpath("//parameter[name=\'Java\']/value").content = %x$java -version$.match(/[\d]*\.[\d]*\.[\d]*/)
   f.puts doc
 end
 
